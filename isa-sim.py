a16a32d84aabd61dd80ace538103b1e6eb516b9b
Paulo9608 <71069676+Paulo9608@users.noreply.github.com>
2020-10-04 17:02:50 +0200

    ahah

--- isa-sim.py
+++ isa-sim.py
@@ -313,16 +313,16 @@ print('\n---Start of simulation---')
 
 def read_instruction(program_counter):
     instruction = instructionMemory.read_opcode(program_counter)
-    if instruction in instructions['3_arguments']:
+    if instruction in instructions[3]:
         op_1 = instructionMemory.read_operand_1(program_counter)
         op_2 = instructionMemory.read_operand_2(program_counter)
         op_3 = instructionMemory.read_operand_3(program_counter)
         return instruction, op_1, op_2, op_3
-    elif instruction in instructions['2_arguments']:
+    elif instruction in instructions[2]:
         op_1 = instructionMemory.read_operand_1(program_counter)
         op_2 = instructionMemory.read_operand_2(program_counter)
         return instruction, op_1, op_2
-    elif instruction in instructions['1_arguments']:
+    elif instruction in instructions[1]:
         op_1 = instructionMemory.read_operand_1(program_counter)
         return instruction, op_1
     else:
@@ -332,11 +332,11 @@ def read_instruction(program_counter):
 def PrintStateInfo():
     print(f"Current cycle #{current_cycle}:")
     print(f"Current program counter: {program_counter}")
-    print(f"Instruction being executed: {instruction}")
+    print(f"Instruction being executed: {instructionl}")
     
 
 instructions = {
-    '3_arguments':{
+    3 :{
         'ADD' : (lambda R, I, program_counter, reg_1, reg_2, reg_3 : 
                 R.write_register(reg_1,(R.read_register(reg_2) + R.read_register(reg_3)))),      
         'SUB' : (lambda R, I, program_counter, reg_1, reg_2, reg_3 : 
@@ -353,21 +353,21 @@ instructions = {
                  if R.read_register(reg_2) < R.read_register(reg_3) else program_counter)
                   
     },
-    '2_arguments' : {
+    2 : {
         'LI' : (lambda R, D, I, reg, constant: R.write_register(reg,int(constant))),
         'LD' : (lambda R, D, I, reg_1, reg_2 : R.write_register(reg_1, D.read_data(R.read_register(reg_2)))),
-        'SD' : (lambda R, D, I, reg_1, reg_2  : 
+        'SD' : (lambda R, D, I, reg_1, reg_2 : 
                 dataMemory.write_data(R.read_register(reg_2),R.read_register(reg_1))),
         'NOT' :(lambda R, D, I, reg_1, reg_2: 
-                R.write_register(reg_1,(R.read_register(~reg_2))))
+                R.write_register(reg_1,(~R.read_register(reg_2))))
     },
-    '1_argument' : {
+    1 : {
         'JR' : (lambda R, I, program_counter, reg_1, reg_2, reg_3 :
                 program_counter * 0 + R.read_register(reg_1))
     },
-    'no_arguments' : {
-        'NOP': (lambda R, I, program_counter, reg_1, reg_2, reg_3 : program_counter),
-        'END': (lambda R, I, program_counter, reg_1, reg_2, reg_3 : program_counter)
+    0 : {
+        'NOP': (lambda program_counter: program_counter),
+        'END': (lambda program_counter : program_counter)
             
     }
 }
@@ -381,10 +381,26 @@ instructions = {
 
 for current_cycle in range(max_cycles):
     instructionl = list(read_instruction(program_counter))
+    print(instructionl)
+    if len(instructionl) == 4:
+        instructions[3][instructionl[0]](registerFile, instructionMemory, program_counter,
+                                            instructionl[1], instructionl[2], instructionl[3])
+    if len(instructionl) == 3:
+        instructions[2][instructionl[0]](registerFile, dataMemory, instructionMemory,
+                                            instructionl[1], instructionl[2])
+    if len(instructionl) == 2:
+        instructions[1][instructionl[0]](registerFile, instructionMemory, program_counter,
+                                            instructionl[1])
+    if len(instructionl) == 1:
+        instrutionl = "".join(map(str,instructionl))
+        instructions[0][instructionl](program_counter)
+        program_counter -= 1
     
- 
-print(instructionl)
+    program_counter += 1
+    PrintStateInfo()
 
+registerFile.print_all()
+dataMemory.print_all()
     
 #
 
